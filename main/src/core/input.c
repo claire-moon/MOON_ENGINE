2b249ce81384cc9b71abc8115abb0f35041f8f16
Claire Moon <clairemoonmusic.protonmail.com>
2025-03-07 18:16:19 -0600

    update part 2

--- main/src/core/input.c
+++ main/src/core/input.c
@@ -1,33 +1,39 @@
 #include "input.h"
 #include "camera.h"
+#include <SDL2/SDL.h>
+
+//define movement dir(s)
+#ifndef FORWARD
+#define FORWARD 0
+#define BACKWARD 1
+#define LEFT 2
+#define RIGHT 3
+#endif
 
 void Input_HandleEvents(Engine* engine, Camera* camera, float deltaTime) {
 	SDL_Event event;
 	while (SDL_PollEvent(&event)) {
-		switch (event.type) {
-			case SDL_QUIT:
-				engine->running = 0;
-				break;
-
-		case SDL_KEYDOWN:
+		if (event.type == SDL_QUIT) {
+			engine->running = 0;
+		} else if (SDL_MOUSEMOTION) {
+			float xoffset = event.motion.xrel; //x-axis
+			float yoffset = -event.motion.yrel; //inverted y-axis
+			Camera_ProcessMouseMovement(camera, xoffset, yoffset, 1);
+		} else if (event.type == SDL_KEYDOWN) {
 			switch (event.key.keysym.sym) {
 				case SDLK_w:
-					Camera_ProcessKeyboard(camera, 0, deltaTime);
+					Camera_ProcessKeyboard(camera, FORWARD, deltaTime);
 					break;
 				case SDLK_s:
-					Camera_ProcessKeyboard(camera, 1, deltaTime);
+					Camera_ProcessKeyboard(camera, BACKWARD, deltaTime);
 					break;
 				case SDLK_a:
-					Camera_ProcessKeyboard(camera, 2, deltaTime);
+					Camera_ProcessKeyboard(camera, LEFT, deltaTime);
 					break;
 				case SDLK_d:
-					Camera_ProcessKeyboard(camera, 3, deltaTime);	
+					Camera_ProcessKeyboard(camera, RIGHT, deltaTime);
+					break;
 			}
-			break;
-
-		case SDL_MOUSEMOTION:
-			Camera_ProcessMouseMovement(camera, event.motion.xrel, -event.motion.yrel, 1);
-			break;
 		}
 	}
 }
--- main/src/shaders/fragment_shader.glsl
+++ main/src/shaders/fragment_shader.glsl
@@ -5,13 +5,16 @@ in vec3 FragPos;
 in vec3 Normal;
 
 uniform vec3 lightPos;
+uniform sampler2D texture1;
 
-void main() {
-	
+void main() {	
 	//diffuse lighting
 	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(Normal, lightDir), 0.0f);
 	vec3 diffuse = diff * vec3(1.0, 0.5, 0.2); //orange
+	
+	//tex color
+	vec3 texColor = texture(texture1, FragPos.xy).rgb;
 	
 	FragColor = vec4(diffuse, 1.0);
 }
